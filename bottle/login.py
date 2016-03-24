f50d4366a339144b7d8c1a7bf834d6a71feea774
dnogueira@intranet.techsquare.com <dnogueira@intranet.techsquare.com>
2016-03-24 13:48:51 -0400

    bottle

---
+++ bottle/login.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+from bottle import get, post, request
+
+@get('/login') 
+#@route('/login')
+def login():
+    return '''
+        <form action="/login" method="post">
+            Username: <input name="username" type="text" />
+            Password: <input name="password" type="password" />
+            <input value="Login" type="submit" />
+        </form>
+    '''
+
+@post('/login') 
+#@route('/login', method='POST')
+def do_login():
+    username = request.forms.get('username')
+    password = request.forms.get('password')
+    if check_login(username, password):
+        return "<p>Your login information was correct.</p>"
+    else:
+        return "<p>Login failed.</p>"
+
+run(host='localhost', port=8080, debug=True)
